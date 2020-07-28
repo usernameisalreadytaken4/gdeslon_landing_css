d4ccce52ef67469b3d91144cd9f14ec277d387c1
ArhStrAngeR <arhstranger@gmail.com>
2020-07-28 15:15:35 +0300

    Update new_js.js

--- new_js.js
+++ new_js.js
@@ -1,7 +1,15 @@
 $(function() {
 
-  $(".bar").click(function(){
-      $('#navbarMain').toggleClass('d-block');
-  });
+	$(".bar").click(function(){
+		$('#navbarMain').toggleClass('d-block');
+	});
+	
+	$(document).mouseup(function (e){
+		var div = $('.navbar-main');
+		if (!div.is(e.target)
+			&& div.has(e.target).length === 0) {
+			$('#navbarMain').removeClass('d-block');
+		}
+	});
   
 });
